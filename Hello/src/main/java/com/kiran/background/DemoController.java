11859d69d1274cefa5a5c491eaa7227237a5f01f
kiran <kiranpatil123>
2020-09-03 13:14:38 +0530

    New STS

---
+++ Hello/src/main/java/com/kiran/background/DemoController.java
@@ -0,0 +1,14 @@
+package com.kiran.background;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@GetMapping("/hello")
+	public String myMethod() {
+		return "Hi welcome Kiran";
+	}
+
+}
---
+++ Hello/src/test/java/com/kiran/background/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiran.background;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
